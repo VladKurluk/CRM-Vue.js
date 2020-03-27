742e3d13d618886e3bf60021437a2de212d4166f
Vladislav Kurluk <vlad11kurluk89@gmail.com>
2020-03-27 11:55:26 +0200

    Added route protection for unauthorized users

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
   'logout': 'Вы вышли из системы',
+  'login': 'Нужно войти в систему',
   'auth/user-not-found': 'Пользователя не существует',
   'auth/wrong-password': 'Неверный пароль',
   'auth/email-already-in-use': 'Этот Email у другого пользователя'
